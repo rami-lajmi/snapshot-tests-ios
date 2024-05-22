8bdff37c9a0f2b08652fcef3ba57197b7c7e125d
Rami LAJMI <rami.lajmi@soprasteria.com>
2024-05-22 10:27:47 +0200

    Initial Commit

---
+++ Snapshot-ios-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snapshot-ios-sample
+//
+//  Created by Rami LAJMI on 22/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snapshot-ios-sample/Snapshot_ios_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snapshot_ios_sampleApp.swift
+//  Snapshot-ios-sample
+//
+//  Created by Rami LAJMI on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Snapshot_ios_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
